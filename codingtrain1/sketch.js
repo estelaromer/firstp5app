1211ef111c787dae7bef62d95f29adc75d2f2acf
Estela Romero <estelaromer@gmail.com>
2018-07-28 06:54:21 +0200

    Add files via upload

---
+++ codingtrain1/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+	createCanvas(1200, 1000);
+}
+
+function draw() {
+	//console.log(mouseX);
+	ellipse(480,70,30);
+	ellipse(420,70,30);
+	ellipse(450,90,60);
+	line(420,80,480,80);
+}
